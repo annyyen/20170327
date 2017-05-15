46691b26045789e78e6da3d24e8b28260e0e822d
annyyen <anny_yyj@yahoo.com.tw>
2017-05-15 18:53:38 +0800

    1111

---
+++ melt.R
@@ -0,0 +1,34 @@
+#install.packages("readr")
+library(readr)
+hospitalnew <- read_csv("hospitalnew.csv")
+
+# View(hospitalnew)
+# str(hospitalnew)
+
+#篩選科別
+category_DF <- hospitalnew[grep("兒科|婦產科|不分科|家醫科|耳鼻喉科", hospitalnew$診療科別), ]
+
+#切割營業時間
+#install.packages("tidyr")
+library(tidyr)
+separate_DF <- separate(category_DF, 固定看診時段, paste0("T",c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21")))
+                        , sep = "、")
+
+
+# 寬表轉長表
+#install.packages("reshape2")
+library(reshape2)
+setnames(separate_DF,1,"醫事機構代碼")
+long <- melt(separate_DF%>%select(醫事機構代碼,starts_with("T")),id.vars = c("醫事機構代碼"))
+long_DF <- separate_DF%>%gather(test,testvalue, T1:T21)
+
+
+
+
+
+
+
+
+
+
+
--- split.R
+++ split.R
@@ -1,19 +1,39 @@
+#install.packages("readr")
 library(readr)
 hospitalnew <- read_csv("C:/Users/user/Desktop/hospitalnew.csv")
-View(hospitalnew)
+# View(hospitalnew)
+# str(hospitalnew)
 
-strsplit ("星期一上午看診、星期二上午看診、星期三上午看診、星期四上午看診、星期五上午看診、星期六上午看診、星期日上午看診、星期一下午看診、星期二下午看診、星期三下午看診、星期四下午看診、星期五下午看診、星期六下午看診、星期日下午看診、星期一晚上看診、星期二晚上看診、星期三晚上看診、星期四晚上看診、星期五晚上看診、星期六晚上看診、星期日晚上看診
+#篩選科別
+category_DF <- hospitalnew[grep("兒科|婦產科|不分科|家醫科|耳鼻喉科", hospitalnew$診療科別), ]
 
-", "、")
-newfile <- strsplit ("星期一上午看診、星期二上午看診、星期三上午看診、星期四上午看診、星期五上午看診、星期六上午看診、星期日上午看診、星期一下午看診、星期二下午看診、星期三下午看診、星期四下午看診、星期五下午看診、星期六下午看診、星期日下午看診、星期一晚上看診、星期二晚上看診、星期三晚上看診、星期四晚上看診、星期五晚上看診、星期六晚上看診、星期日晚上看診
-
-", "、")
-View(newfile)
-
-
+#切割營業時間
+# install.packages("tidyr")
+library(tidyr)
+separate_DF <- separate(category_DF, 固定看診時段, c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21")
+                        , sep = "、")
 
 
-# library(dplyr)
-# filterVector <- c("中醫", "牙科")
-# filterVector
-# newfile <- filter(hospitalnew, ! c("中醫", "牙科") %in% filterVector)
+# 寬表轉長表
+# install.packages("reshape2")
+# install.packages("dplyr")
+library(reshape2)
+library(dplyr)
+setNames(separate_DF,1,"醫事機構代碼")
+long <- melt(separate_DF%>%select(醫事機構代碼,starts_with("T")),id.vars = c("醫事機構代碼"))
+long_DF <- separate_DF%>%gather(test,testvalue, T1:T21)
+
+
+#long <- melt(separate_DF,id.vars = c("醫事機構代碼","醫事機構名稱","醫事機構種類","電話","地址","分區業務組","特約類別","服務項目","診療科別"))
+#long_DF <- gather(separate_DF, 看診時段, 1:21)
+
+
+
+
+
+
+
+
+
+
+
